e1e5cb3b84a7bbe9365aec7d36d6b9fd93614ec3
elmahdi <elmahdi.ouhssain@gmail.com>
2020-11-24 17:43:50 +0100

    first sync24/11/2020

---
+++ website/home/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Generale(models.Model):
+
+	title = models.CharField(max_length=200)
+	description = models.CharField(max_length=500)
+
+	updated_at = models.DateTimeField('Updated at')
+
+
